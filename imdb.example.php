4fa8b7072d192e5f1a1f1df544e73b1d405bc279
Fabian Beiner <mail@fabian-beiner.de>
2010-01-30 14:41:42 +0100

    Updated the examples to the new improvements.

--- imdb.example.php
+++ imdb.example.php
@@ -1,31 +1,47 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd"
+    >
+<html lang="en">
+<head>
+    <title>IMDB PHP Parser | Examples</title>
+    <style type="text/css">
+        body {background-color:#fff;color:#000;font-family:Verdana,Arial,sans-serif;font-size:13px;}
+    </style>
+</head>
+<body>
 <?php
 include_once 'imdb.class.php';
 
-$oIMDB = new IMDB('Valkyrie');
+$oIMDB = new IMDB('New York, I Love You');
 if ($oIMDB->_bFound) {
-	echo 'Title: ' . $oIMDB->getTitle() . '<br>';
-	echo 'Country: ' . $oIMDB->getCountry() . '<br>';
-	echo 'Country Url: ' . $oIMDB->getCountryUrl() . '<br>';
-	echo 'Director: ' . $oIMDB->getDirector() . '<br>';
-	echo 'Director Url: ' . $oIMDB->getDirectorUrl() . '<br>';
-	echo 'MPAA: ' . $oIMDB->getMpaa() . '<br>';
-	echo 'Plot: ' . $oIMDB->getPlot() . '<br>';
-	echo 'Rating: ' . $oIMDB->getRating() . '<br>';
-	echo 'Release Date: ' . $oIMDB->getReleaseDate() . '<br>';
-	echo 'Runtime: ' . $oIMDB->getRuntime() . '<br>';
-	echo 'Url: ' . $oIMDB->getUrl() . '<br>';
-	echo 'Votes: ' . $oIMDB->getVotes() . '<br>';
-	echo 'Poster Url: ' . $oIMDB->getPoster() . '<br>';
-	echo 'Tagline: ' . $oIMDB->getTagline() . '<br>';
-	echo 'Year: ' . $oIMDB->getYear() . '<br>';
-	echo 'Genre: ' . $oIMDB->getGenre() . '<br>';
+	echo 'Cast: <b>' . $oIMDB->getCast(5) . '</b><br>';
+	echo 'Cast as URL: <b>' . $oIMDB->getCastAsUrl(15) . '</b><br>';
+	echo 'Countries: <b>' . $oIMDB->getCountry() . '</b><br>';
+	echo 'Countries as URL: <b>' . $oIMDB->getCountryAsUrl() . '</b><br>';
+	echo 'Directors: <b>' . $oIMDB->getDirector() . '</b><br>';
+	echo 'Directors as URL: <b>' . $oIMDB->getDirectorAsUrl() . '</b><br>';
+	echo 'Genres: <b>' . $oIMDB->getGenre() . '</b><br>';
+	echo 'Genres as URL: <b>' . $oIMDB->getGenreAsUrl() . '</b><br>';
+	echo 'MPAA: <b>' . $oIMDB->getMpaa() . '</b><br>';
+	echo 'Plot: <b>' . $oIMDB->getPlot() . '</b><br>';
+	echo 'Poster: <b>' . $oIMDB->getPoster() . '</b><br>';
+	echo 'Rating: <b>' . $oIMDB->getRating() . '</b><br>';
+	echo 'Release Date: <b>' . $oIMDB->getReleaseDate() . '</b><br>';
+	echo 'Runtime: <b>' . $oIMDB->getRuntime() . '</b><br>';
+	echo 'Tagline: <b>' . $oIMDB->getTagline() . '</b><br>';
+	echo 'Title: <b>' . $oIMDB->getTitle() . '</b><br>';
+	echo 'Url: <b>' . $oIMDB->getUrl() . '</b><br>';
+	echo 'Votes: <b>' . $oIMDB->getVotes() . '</b><br>';
+	echo 'Year: <b>' . $oIMDB->getYear() . '</b><br>';
+	echo 'Writers: <b>' . $oIMDB->getWriter() . '</b><br>';
+	echo 'Writers as URL: <b>' . $oIMDB->getWriterAsUrl() . '</b><br>';
 } else {
 	echo 'Movie not found!';
 }
 
 echo '<hr>';
 
-$oIMDB = new IMDB('http://us.imdb.com/Title?0118883');
+$oIMDB = new IMDB('http://us.imdb.com/Title?0144117');
 if ($oIMDB->_bFound) {
 	echo '<a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.';
 } else {
@@ -34,11 +50,11 @@ if ($oIMDB->_bFound) {
 
 echo '<hr>';
 
-$oIMDB = new IMDB('http://www.imdb.com/title/tt0478087/');
+$oIMDB = new IMDB('http://www.imdb.com/title/tt1022603/');
 if ($oIMDB->_bFound) {
 	echo '<a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.<br>';
-	echo 'About the movie: <b>' . $oIMDB->getPlot() . '"</b><br>';
-	echo '<img src="' . $oIMDB->getPoster() . '">' . '<br>';
+	echo 'About the movie: <b>' . $oIMDB->getPlot() . '</b><br>';
+	echo '<img src="' . $oIMDB->getPoster() . '">' . '</b><br>';
 } else {
 	echo 'Movie not found!';
 }
@@ -47,7 +63,10 @@ echo '<hr>';
 
 $oIMDB = new IMDB('Fabian Beiner never made a movie. Yet!');
 if ($oIMDB->_bFound) {
-	echo $oIMDB->getTitle() . '<br>';
+	echo $oIMDB->getTitle() . '</b><br>';
 } else {
 	echo 'Movie not found!';
 }
+?>
+</body>
+</html>
