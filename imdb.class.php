13a1605d4f986fcfa6cea88c7e31c077dac5b240
Fabian Beiner <mail@fabian-beiner.de>
2010-10-08 20:09:59 +0200

    Renamed 'redirects' to 'cache'. Added caching for requests. Removed /10 from rating.

--- imdb.class.php
+++ imdb.class.php
@@ -16,11 +16,7 @@
  * @author Fabian Beiner (mail@fabian-beiner.de)
  * @license MIT License
  *
- * @version 5.0.0a (October 8th, 2010)
- *
- * @todo
- * - Add a cURL caching mechanism
- * - ...
+ * @version 5.0.1 (October 8th, 2010)
 */
 
 class IMDB {
@@ -60,6 +56,12 @@ class IMDB {
     private $strSource = NULL;
     // IMDB id
     private $intId     = NULL;
+    // IMDB cache
+    private $intCache  = 0;
+    // IMDB posters directory
+    private $bolPoster = NULL;
+    // IMDB cache directory
+    private $bolCache  = NULL;
 
     // Movie found?
     public $isReady    = false;
@@ -70,16 +72,43 @@ class IMDB {
      * @param string  $strSearch The movie name / IMDB url
      * @param integer $intCache  The maximum age (in minutes) of a cache
      */
-    public function __construct($strSearch, $intCache = 0) {
+    public function __construct($strSearch, $intCache = 1440) {
+        // Cookie path.
         if (function_exists('sys_get_temp_dir')) {
             $this->fCookie = tempnam(sys_get_temp_dir(), 'imdb');
             if (IMDB::IMDB_DEBUG) echo '<b>- Path to cookie:</b> ' . $this->fCookie . '<br>';
         }
+        // Posters and cache directory existant?
+        if (!file_exists(getcwd() . '/posters/')) {
+            if (mkdir(getcwd() . '/posters/', 0777)) {
+                $this->bolPoster = true;
+            }
+        }
+        elseif (is_writeable(getcwd() . '/posters/')) {
+            $this->bolPoster = true;
+        }
+        else {
+            return;
+        }
+        if (!file_exists(getcwd() . '/cache/')) {
+            if (mkdir(getcwd() . '/cache/', 0777)) {
+                $this->bolCache = true;
+            }
+        }
+        elseif (is_writeable(getcwd() . '/cache/')) {
+            $this->bolCache = true;
+        }
+        else {
+            return;
+        }
+        // Debug only.
         if (IMDB::IMDB_DEBUG) {
           error_reporting(E_ALL);
           ini_set('display_errors', '1');
           echo '<b>- Running:</b> IMDB::fetchUrl<br>';
         }
+        // Set global cache and fetch the data.
+        $this->intCache = (int)$intCache;
         IMDB::fetchUrl($strSearch);
     }
 
@@ -122,11 +151,10 @@ class IMDB {
      * Fetch data from the given url.
      *
      * @param string  $strSearch The movie name / IMDB url
-     * @param integer $intCache  The maximum age (in minutes) of a cache
      * @param string  $strSave   The path to the file
      * @return boolean
      */
-    private function fetchUrl($strSearch, $intCache = 0) {
+    private function fetchUrl($strSearch) {
         // Remove whitespaces.
         $strSearch = trim($strSearch);
 
@@ -148,8 +176,27 @@ class IMDB {
             }
         }
 
+        $fCache = getcwd() . '/cache/' . md5($this->strUrl) . '.cache';
+
+        // Check if there is a cache we can use.
+        $bolNewRequest = false;
+        if (file_exists($fCache)) {
+            $intChanged = filemtime($fCache);
+            $intNow     = time();
+            $intDiff    = $intNow - $intChanged;
+            $intCache   = $this->intCache * 60;
+            if ($intCache >= $intDiff) {
+                $bolNewRequest = true;
+            }
+        }
+
+        if ($bolNewRequest) {
+            if (IMDB::IMDB_DEBUG) echo '<b>- Using cache for ' . $strSearch . ' from . ' . $fCache . '</b><br>';
+            $this->strSource = file_get_contents($fCache);
+            return true;
+        }
         // Check if cURL is installed.
-        if (function_exists('curl_init')) {
+        elseif (function_exists('curl_init')) {
             // Initialize and run the request.
             if (IMDB::IMDB_DEBUG) echo '<b>- Run cURL on:</b> ' . $this->strUrl . '<br>';
             $oCurl = curl_init($this->strUrl);
@@ -171,6 +218,10 @@ class IMDB {
             // Check if the request actually worked.
             if ($strOutput === FALSE) {
                 if (IMDB::IMDB_DEBUG) echo '<b>! cURL error:</b> ' . $strUrl . '<br>';
+                if (file_exists($fCache)) {
+                     $this->strSource = file_get_contents($fCache);
+                    return true;
+                }
                 return;
             }
 
@@ -183,12 +234,10 @@ class IMDB {
                 if (IMDB::IMDB_DEBUG) echo '<b>- Found a redirect:</b> ' . $strMatch . '<br>';
                 // Try to save the redirect for later usage.
                 $fRedirect = getcwd() . '/cache/' . md5($this->strUrl);
-                if (is_dir(getcwd() . '/cache')) {
-                    if (IMDB::IMDB_DEBUG) echo '<b>- Saved a new redirect:</b> ' . $strMatch . '<br>';
-                    file_put_contents($fRedirect, $strMatch);
-                }
+                if (IMDB::IMDB_DEBUG) echo '<b>- Saved a new redirect:</b> ' . $fRedirect . '<br>';
+                file_put_contents($fRedirect, $strMatch);
                 // Run the cURL request again with the new url.
-                IMDB::fetchUrl($strMatch, $intCache);
+                IMDB::fetchUrl($strMatch);
             }
             // Lets assume the first search result is what we want. :)
             elseif ($strMatch = $this->matchRegex($strOutput, IMDB::IMDB_SEARCH, 1)) {
@@ -196,12 +245,10 @@ class IMDB {
                 if (IMDB::IMDB_DEBUG) echo '<b>- Using the first search result:</b> ' . $strMatch . '<br>';
                 // Try to save the redirect for later usage.
                 $fRedirect = getcwd() . '/cache/' . md5($this->strUrl);
-                if (is_dir(getcwd() . '/cache')) {
-                    if (IMDB::IMDB_DEBUG) echo '<b>- Saved a new redirect:</b> ' . $strMatch . '<br>';
-                    file_put_contents($fRedirect, $strMatch);
-                }
+                if (IMDB::IMDB_DEBUG) echo '<b>- Saved a new redirect:</b> ' . $fRedirect . '<br>';
+                file_put_contents($fRedirect, $strMatch);
                 // Run the cURL request again with the new url.
-                IMDB::fetchUrl($strMatch, $intCache);
+                IMDB::fetchUrl($strMatch);
             }
             // If it's not a redirect and the HTTP response is not 200 or 302, abort.
             elseif ($arrInfo['http_code'] != 200 && $arrInfo['http_code'] != 302) {
@@ -211,6 +258,11 @@ class IMDB {
 
             // Set the global source.
             $this->strSource = preg_replace('~(\r|\n|\r\n)~', '', $this->strSource);
+
+            // Save cache.
+            if (IMDB::IMDB_DEBUG) echo '<b>- Saved a new cache:</b> ' . $fCache . '<br>';
+            file_put_contents($fCache, $this->strSource);
+
             return true;
         }
         return;
@@ -233,40 +285,30 @@ class IMDB {
             return $strUrl;
         }
 
-        $bolDir = false;
-        if (!is_dir(getcwd() . '/posters')) {
-            if (mkdir(getcwd() . '/posters', 0777)) {
-                $bolDir = true;
-            }
-        }
         $strFilename = getcwd() . '/posters/' . md5(IMDB::getTitle()) . '.jpg';
         if (file_exists($strFilename)) {
             return 'posters/' . md5(IMDB::getTitle()) . '.jpg';
         }
-        if (is_dir(getcwd() . '/posters') OR $bolDir) {
-            if (function_exists('curl_init')) {
-
-                $oCurl = curl_init($strUrl);
-                curl_setopt_array($oCurl, array (
-                                                CURLOPT_VERBOSE => FALSE,
-                                                CURLOPT_HEADER => FALSE,
-                                                CURLOPT_RETURNTRANSFER => TRUE,
-                                                CURLOPT_TIMEOUT => IMDB::IMDB_TIMEOUT,
-                                                CURLOPT_CONNECTTIMEOUT => IMDB::IMDB_TIMEOUT,
-                                                CURLOPT_REFERER => $strUrl,
-                                                CURLOPT_BINARYTRANSFER => TRUE));
-                $sOutput = curl_exec($oCurl);
-                curl_close($oCurl);
-                $oFile = fopen($strFilename, 'x');
-                fwrite($oFile, $sOutput);
-                fclose($oFile);
-                return 'posters/' . md5(IMDB::getTitle()) . '.jpg';
-            } else {
-                $oImg = imagecreatefromjpeg($strUrl);
-                imagejpeg($oImg, $strFilename);
-                return 'posters/' . md5(IMDB::getTitle()) . '.jpg';
-            }
-            return $strUrl;
+        if (function_exists('curl_init')) {
+            $oCurl = curl_init($strUrl);
+            curl_setopt_array($oCurl, array (
+                                            CURLOPT_VERBOSE => FALSE,
+                                            CURLOPT_HEADER => FALSE,
+                                            CURLOPT_RETURNTRANSFER => TRUE,
+                                            CURLOPT_TIMEOUT => IMDB::IMDB_TIMEOUT,
+                                            CURLOPT_CONNECTTIMEOUT => IMDB::IMDB_TIMEOUT,
+                                            CURLOPT_REFERER => $strUrl,
+                                            CURLOPT_BINARYTRANSFER => TRUE));
+            $sOutput = curl_exec($oCurl);
+            curl_close($oCurl);
+            $oFile = fopen($strFilename, 'x');
+            fwrite($oFile, $sOutput);
+            fclose($oFile);
+            return 'posters/' . md5(IMDB::getTitle()) . '.jpg';
+        } else {
+            $oImg = imagecreatefromjpeg($strUrl);
+            imagejpeg($oImg, $strFilename);
+            return 'posters/' . md5(IMDB::getTitle()) . '.jpg';
         }
         return $strUrl;
     }
