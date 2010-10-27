c3b17b35ac8955c22f86517130e5bcfa3845bcd7
Fabian Beiner <mail@fabian-beiner.de>
2010-10-27 20:59:36 +0200

    Added new methodes.

--- imdb.tests.php
+++ imdb.tests.php
@@ -30,19 +30,21 @@ include_once 'imdb.class.php';
 $arrTests = array('North by Northwest', 'Iron Man 2', 'One Tree Hill', 'Formosa Betrayed', 'New York, I Love You', 'http://us.imdb.com/Title?0144117', 'http://www.imdb.com/title/tt1022603/', 'Fabian Beiner never made a movie. Yet!');
 
 foreach ($arrTests as $strTest) {
-    $oIMDB = new IMDB($strTest, 1);
+    $oIMDB = new IMDB($strTest, 10);
     if ($oIMDB->isReady) {
         echo '<p>Budget: <b>' . $oIMDB->getBudget() . '</b></p>';
         echo '<p>Cast (limited to 5): <b>' . $oIMDB->getCast(5) . '</b></p>';
         echo '<p>Cast as URL (default limited to 20): <b>' . $oIMDB->getCastAsUrl() . '</b></p>';
-        echo '<p>Countries: <b>' . $oIMDB->getCountry() . '</b></p>';
         echo '<p>Countries as URL: <b>' . $oIMDB->getCountryAsUrl() . '</b></p>';
-        echo '<p>Directors: <b>' . $oIMDB->getDirector() . '</b></p>';
+        echo '<p>Countries: <b>' . $oIMDB->getCountry() . '</b></p>';
         echo '<p>Directors as URL: <b>' . $oIMDB->getDirectorAsUrl() . '</b></p>';
-        echo '<p>Genres: <b>' . $oIMDB->getGenre() . '</b></p>';
+        echo '<p>Directors: <b>' . $oIMDB->getDirector() . '</b></p>';
         echo '<p>Genres as URL: <b>' . $oIMDB->getGenreAsUrl() . '</b></p>';
-        echo '<p>Languages: <b>' . $oIMDB->getLanguages() . '</b></p>';
+        echo '<p>Genres: <b>' . $oIMDB->getGenre() . '</b></p>';
         echo '<p>Languages as URL: <b>' . $oIMDB->getLanguagesAsUrl() . '</b></p>';
+        echo '<p>Languages: <b>' . $oIMDB->getLanguages() . '</b></p>';
+        echo '<p>Location as URL: <b>' . $oIMDB->getLocationAsUrl() . '</b></p>';
+        echo '<p>Location: <b>' . $oIMDB->getLocation() . '</b></p>';
         echo '<p>MPAA: <b>' . $oIMDB->getMpaa() . '</b></p>';
         echo '<p>Plot (shortened to 150 chars): <b>' . $oIMDB->getPlot(150) . '</b></p>';
         echo '<p>Poster: <b>' . $oIMDB->getPoster() . '</b></p>';
@@ -53,9 +55,6 @@ foreach ($arrTests as $strTest) {
         echo '<p>Title: <b>' . $oIMDB->getTitle() . '</b></p>';
         echo '<p>Url: <b><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getUrl() . '</a></b></p>';
         echo '<p>Votes: <b>' . $oIMDB->getVotes() . '</b></p>';
-        echo '<p>Year: <b>' . $oIMDB->getYear() . '</b></p>';
-        echo '<p>Writers: <b>' . $oIMDB->getWriter() . '</b></p>';
-        echo '<p>Writers as URL: <b>' . $oIMDB->getWriterAsUrl() . '</b></p>';
         echo '<hr>';
     }
     else {
