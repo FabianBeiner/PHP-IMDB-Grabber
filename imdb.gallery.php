258bec56d5cac0b9297243f489f63e1eb9f82585
Fabian Beiner <mail@fabian-beiner.de>
2010-12-20 23:40:05 +0100

    "css effects and a small correction" - by xsabianus

--- imdb.gallery.php
+++ imdb.gallery.php
@@ -4,10 +4,44 @@
   <meta charset="utf-8" />
   <title>PHP-IMDB-Grabber by Fabian Beiner | Gallery by xsabianus</title>
   <!-- Small modifications by Fabian Beiner. Anyhow, no support for this script. -->
+<style>
+
+img{
+border:5px  solid #ECEBEB;
+margin:4px 8px 5px 0;
+-moz-border-radius:5px 5px 5px 5px;
+-khtml-border-radius:5px 5px 5px 5px;
+-webkit-border-radius:5px 5px 5px 5px;
+border-radius:5px 5px 5px 5px;  
+  }
+  
+a  img {
+display:inline;
+margin-bottom:10px;
+margin-right:5px;
+margin-left:5px;
+background:#f5f5f5;
+border:1px solid #ccc;
+padding:2px;
+}
+
+a:hover img {
+background:#4680C2;
+border:1px  solid #4680C2;
+}
+img:hover {
+opacity: .75;
+-moz-opacity: .75;
+filter: alpha(opacity=75);
+
+}
+
+ </style>
+
 </head>
 <body>
-<table width="655" bgcolor="#ffffff" border="0" cellspacing="8" cellpadding="8" align="center">
-<table width="655" bgcolor="#ffffff" border="0" cellspacing="8" cellpadding="8" align="center">
+<table width="760" bgcolor="#ffffff" border="0" cellspacing="8" cellpadding="8" align="center">
+<table width="760" bgcolor="#ffffff" border="0" cellspacing="8" cellpadding="8" align="center">
 <td>
   <center>
 <?php
@@ -20,7 +54,7 @@ $picture[] = $picturefile;
 closedir($take);
 
 
-$limit = 30; //Number of images to display on a page
+$limit = 24; //Number of images to display on a page
 $page = $_GET["page"];
 if($page < 1) $page = 1;
 $total = count($picture);
@@ -35,7 +69,7 @@ for($i=$startpage; $i < $endpage; $i++){
 echo "
 
 <a href='".$root."/".$picture[$i]."' target='_blank'>
-<img onContextMenu='return false' src='".$root."/".$picture[$i]."' width='100'  height='100' border='0'></a>";
+<img  onContextMenu='return false' src='".$root."/".$picture[$i]."' width='100'  height='100' border='0'></a>";
 }
 echo"<br>";
 
@@ -44,7 +78,7 @@ for($i=1; $i < $total / $limit; $i++){
 if($page == $i)
 
 echo "$in"; else
-echo "<a href='gallery.php?page=$i'>[$i]</a> ";
+echo "<a href='imdb.gallery.php?page=$i'>[$i]</a>";
 
 }
 
