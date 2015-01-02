340e6400649ecfee0c7bc392724a8ed75864f19e
Fabian Beiner <fb@fabianbeiner.de>
2015-01-02 18:21:16 +0100

    Updated examples and tests.

--- examples/imdb.example.php
+++ examples/imdb.example.php
@@ -1,90 +1,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <title>PHP-IMDB-Grabber by Fabian Beiner | Examples</title>
-  <style>
-    body {
-      background-color:#aaa;
-      color:#111;
-      font-family:Corbel, "Lucida Grande", "Lucida Sans Unicode",  "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, sans-serif;
-      font-size:14px;
-      margin:20px auto;
-      width:700px;
-    }
-    p {
-      margin:0;
-      padding:0;
-      margin-bottom:5px;
-    }
-    hr {
-      clear:both;
-      margin:20px 0;
-    }
-  </style>
+    <meta charset="utf-8">
+    <title>PHP-IMDB-Grabber by Fabian Beiner | Example</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,700">
+    <style>
+        body {
+            background-color: #E5E5E5;
+            color:            #222;
+            font-family:      "Open Sans", sans-serif;
+            font-size:        15px;
+            max-width:        1000px;
+            margin:           20px auto;
+            width:            100%;
+        }
+
+        p {
+            margin:  0 0 10px;
+            padding: 0;
+        }
+
+        hr {
+            clear:      both;
+            margin:     25px 0;
+            border:     1px #000 solid;
+            height:     1px;
+            background: #FFF;
+        }
+
+        a {
+            color: #222;
+        }
+
+        a:hover, a:focus, a:active {
+            text-decoration: none;
+            color:           #222;
+        }
+
+        h1 {
+            font-size:   32px;
+            text-align:  center;
+            font-weight: 700;
+        }
+    </style>
 </head>
 <body>
 <?php
-include_once 'imdb.class.php';
+include_once '../imdb.class.php';
 
-$oIMDB = new IMDB('New York, I Love You');
+$oIMDB = new IMDB('http://us.imdb.com/Title?0144117');
 if ($oIMDB->isReady) {
-        echo '<p>Also Known As: <b>' . $oIMDB->getAka() . '</b></p>' . PHP_EOL;
-        echo '<p>Aspect Ratio: <b>' . $oIMDB->getAspectRatio() . '</b></p>' . PHP_EOL;
-        echo '<p>Budget: <b>' . $oIMDB->getBudget() . '</b></p>' . PHP_EOL;
-        echo '<p>Cast: <b>' . $oIMDB->getCast() . '</b></p>' . PHP_EOL;
-        echo '<p>Full Cast: <b>' . $oIMDB->getFullCast() . '</b></p>' . PHP_EOL;
-        echo '<p>Cast as URL: <b>' . $oIMDB->getCastAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Cast and Character: <b>' . $oIMDB->getCastAndCharacter() . '</b></p>' . PHP_EOL;
-        echo '<p>Cast and Character as URL: <b>' . $oIMDB->getCastAndCharacterAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Color: <b>' . $oIMDB->getColor() . '</b></p>' . PHP_EOL;
-        echo '<p>Company: <b>' . $oIMDB->getCompany() . '</b></p>' . PHP_EOL;
-        echo '<p>Company as URL: <b>' . $oIMDB->getCompanyAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Countries: <b>' . $oIMDB->getCountry() . '</b></p>' . PHP_EOL;
-        echo '<p>Countries as URL: <b>' . $oIMDB->getCountryAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Creators: <b>' . $oIMDB->getCreator() . '</b></p>' . PHP_EOL;
-        echo '<p>Creators as URL: <b>' . $oIMDB->getCreatorAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Description: <b>' . $oIMDB->getDescription() . '</b></p>' . PHP_EOL;
-        echo '<p>Directors: <b>' . $oIMDB->getDirector() . '</b></p>' . PHP_EOL;
-        echo '<p>Directors as URL: <b>' . $oIMDB->getDirectorAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Genres: <b>' . $oIMDB->getGenre() . '</b></p>' . PHP_EOL;
-        echo '<p>Genres as URL: <b>' . $oIMDB->getGenreAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Languages: <b>' . $oIMDB->getLanguages() . '</b></p>' . PHP_EOL;
-        echo '<p>Languages as URL: <b>' . $oIMDB->getLanguagesAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Location: <b>' . $oIMDB->getLocation() . '</b></p>' . PHP_EOL;
-        echo '<p>Location as URL: <b>' . $oIMDB->getLocationAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>MPAA: <b>' . $oIMDB->getMpaa() . '</b></p>' . PHP_EOL;
-        echo '<p>Opening Weekend: <b>' . $oIMDB->getOpening() . '</b></p>' . PHP_EOL;
-        echo '<p>Plot: <b>' . $oIMDB->getPlot() . '</b></p>' . PHP_EOL;
-        echo '<p>Poster: <b>' . $oIMDB->getPoster() . '</b></p>' . PHP_EOL;
-        echo '<p>Rating: <b>' . $oIMDB->getRating() . '</b></p>' . PHP_EOL;
-        echo '<p>Release Date: <b>' . $oIMDB->getReleaseDate() . '</b></p>' . PHP_EOL;
-        echo '<p>Runtime: <b>' . $oIMDB->getRuntime() . '</b></p>' . PHP_EOL;
-        echo '<p>Seasons: <b>' . $oIMDB->getSeasons() . '</b></p>' . PHP_EOL;
-        echo '<p>Sound Mix: <b>' . $oIMDB->getSoundMix() . '</b></p>' . PHP_EOL;
-        echo '<p>Tagline: <b>' . $oIMDB->getTagline() . '</b></p>' . PHP_EOL;
-        echo '<p>Title: <b>' . $oIMDB->getTitle() . '</b></p>' . PHP_EOL;
-        echo '<p>Trailer: <b><a href="' . $oIMDB->getTrailerAsUrl() . '">' . $oIMDB->getTrailerAsUrl() . '</a></b></p>' . PHP_EOL;
-        echo '<p>Url: <b><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getUrl() . '</a></b></p>' . PHP_EOL;
-        echo '<p>Votes: <b>' . $oIMDB->getVotes() . '</b></p>' . PHP_EOL;
-        echo '<p>Writers: <b>' . $oIMDB->getWriter() . '</b></p>' . PHP_EOL;
-        echo '<p>Writers as URL: <b>' . $oIMDB->getWriterAsUrl() . '</b></p>' . PHP_EOL;
-        echo '<p>Year: <b>' . $oIMDB->getYear() . '</b></p>' . PHP_EOL;
+    echo '<p><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.</p>';
 }
 else {
-    echo '<p>Movie not found!</p>' . PHP_EOL;
+    echo '<p>Movie not found!</p>';
 }
 ?>
 
 <hr>
 
 <?php
-$oIMDB = new IMDB('http://us.imdb.com/Title?0144117');
+$oIMDB = new IMDB('New York, I Love You');
 if ($oIMDB->isReady) {
-    echo '<p><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.</p>' . PHP_EOL;
+    echo '<h1>' . $oIMDB->getTitle() . '</h1>';
+    foreach ($oIMDB->getAll() as $aItem) {
+        if ($oIMDB->sNotFound !== $aItem['value']) {
+            echo '<p><b>' . $aItem['name'] . '</b>: ' . $aItem['value'] . '</p>';
+        }
+    }
 }
 else {
-    echo '<p>Movie not found!</p>' . PHP_EOL;
+    echo '<p>Movie not found!</p>';
 }
 ?>
 
@@ -93,11 +79,11 @@ else {
 <?php
 $oIMDB = new IMDB('http://www.imdb.com/title/tt1022603/');
 if ($oIMDB->isReady) {
-    echo '<p><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.</p>' . PHP_EOL;
-    echo '<p><img src="' . $oIMDB->getPoster() . '" style="float:left;margin:4px 10px 10px 0;"> <b>About the movie:</b> ' . $oIMDB->getPlot() . '</p>' . PHP_EOL;
+    echo '<p><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.</p>';
+    echo '<p><img src="' . $oIMDB->getPoster() . '" style="float:left;margin:4px 10px 10px 0;"> <b>About the movie:</b> ' . $oIMDB->getPlot() . '</p>';
 }
 else {
-    echo '<p>Movie not found!</p>' . PHP_EOL;
+    echo '<p>Movie not found!</p>';
 }
 ?>
 
@@ -106,10 +92,10 @@ else {
 <?php
 $oIMDB = new IMDB('Fabian Beiner never made a movie. Yet!');
 if ($oIMDB->isReady) {
-    echo '<p><b>' . $oIMDB->getTitle() . '</b></p>' . PHP_EOL;
+    echo '<p><b>' . $oIMDB->getTitle() . '</b></p>';
 }
 else {
-    echo '<p>Movie not found!</p>' . PHP_EOL;
+    echo '<p>Movie not found!</p>';
 }
 ?>
 </body>
--- examples/imdb.tests.php
+++ examples/imdb.tests.php
@@ -1,94 +1,87 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <title>PHP-IMDB-Grabber by Fabian Beiner | Tests</title>
-  <style>
-    body {
-      background-color:#aaa;
-      color:#111;
-      font-family:Corbel, "Lucida Grande", "Lucida Sans Unicode",  "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, sans-serif;
-      font-size:14px;
-      margin:20px auto;
-      width:700px;
-    }
-    p {
-      margin:0;
-      padding:0;
-      margin-bottom:5px;
-    }
-    hr {
-      clear:both;
-      margin:20px 0;
-    }
-  </style>
+    <meta charset="utf-8">
+    <title>PHP-IMDB-Grabber by Fabian Beiner | Tests</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,700">
+    <style>
+        body {
+            background-color: #E5E5E5;
+            color:            #222;
+            font-family:      "Open Sans", sans-serif;
+            font-size:        15px;
+            max-width:        1000px;
+            margin:           20px auto;
+            width:            100%;
+        }
+
+        p {
+            margin:  0 0 10px;
+            padding: 0;
+        }
+
+        hr {
+            margin:     25px 0;
+            border:     1px #000 solid;
+            height:     1px;
+            background: #FFF;
+        }
+
+        a {
+            color: #222;
+        }
+
+        a:hover, a:focus, a:active {
+            text-decoration: none;
+            color:           #222;
+        }
+
+        h1 {
+            font-size:   32px;
+            text-align:  center;
+            font-weight: 700;
+        }
+    </style>
 </head>
 <body>
 <?php
 include_once '../imdb.class.php';
 
-$arrTests = array('http://www.imdb.com/title/tt0421974/', 'http://www.imdb.com/title/tt0094618/', 'http://www.imdb.com/title/tt0448157/', 'Matrix', 'Donner Pass', 'If only', 'http://www.imdb.com/title/tt1604113/', 'Wyse Guys', 'http://www.imdb.com/title/tt2005268/', 'Wer ist Clark Rockefeller?', 'North by Northwest', 'Iron Man 2', 'One Tree Hill', 'Formosa Betrayed', 'New York, I Love You', 'http://us.imdb.com/Title?0144117', 'http://www.imdb.com/title/tt1022603/', 'Fabian Beiner never made a movie. Yet!');
+$aTests = array(
+    'http://www.imdb.com/title/tt0421974/',
+    'http://www.imdb.com/title/tt0094618/',
+    'http://www.imdb.com/title/tt0448157/',
+    'Matrix',
+    'Donner Pass',
+    'If only',
+    'http://www.imdb.com/title/tt1604113/',
+    'Wyse Guys',
+    'http://www.imdb.com/title/tt2005268/',
+    'Wer ist Clark Rockefeller?',
+    'North by Northwest',
+    'Iron Man 2',
+    'One Tree Hill',
+    'Formosa Betrayed',
+    'New York, I Love You',
+    'http://us.imdb.com/Title?0144117',
+    'http://www.imdb.com/title/tt1022603/',
+    'Fabian Beiner never made a movie. Yet!'
+);
 
-set_time_limit(count($arrTests) * 15);
+set_time_limit(count($aTests) * 15);
 
 $i = 0;
-foreach ($arrTests as $strTest) {
+foreach ($aTests as $sMovie) {
     $i++;
-    $oIMDB = new IMDB($strTest, 120);
-    $oIMDB->strNotFound = 'Not found! :(';
+    $oIMDB = new IMDB($sMovie);
     if ($oIMDB->isReady) {
-        echo '<h1>' . $strTest . '</h1>';
-        echo '<p>Also Known As: <b>' . $oIMDB->getAka() . '</b></p>';
-        echo '<p>Aspect Ratio: <b>' . $oIMDB->getAspectRatio() . '</b></p>';
-        echo '<p>Budget: <b>' . $oIMDB->getBudget() . '</b></p>';
-        echo '<p>Cast (full): <b>' . $oIMDB->getFullCast() . '</b></p>';
-        echo '<p>Cast (limited to 5): <b>' . $oIMDB->getCast(5) . '</b></p>';
-        echo '<p>Cast as URL (default limited to 20): <b>' . $oIMDB->getCastAsUrl(20, true, '_blank') . '</b></p>';
-        echo '<p>Cast and Character (limited to 10): <b>' . $oIMDB->getCastAndCharacter(10) . '</b></p>';
-        echo '<p>Cast and Character as URL (limited to 10): <b>' . $oIMDB->getCastAndCharacterAsUrl(10) . '</b></p>';
-        echo '<p>Color: <b>' . $oIMDB->getColor() . '</b></p>';
-        echo '<p>Company: <b>' . $oIMDB->getCompany() . '</b></p>';
-        echo '<p>Company as URL: <b>' . $oIMDB->getCompanyAsUrl() . '</b></p>';
-        echo '<p>Countries: <b>' . $oIMDB->getCountry() . '</b></p>';
-        echo '<p>Countries as URL: <b>' . $oIMDB->getCountryAsUrl() . '</b></p>';
-        echo '<p>Creators: <b>' . $oIMDB->getCreator() . '</b></p>';
-        echo '<p>Creators as URL: <b>' . $oIMDB->getCreatorAsUrl() . '</b></p>';
-        echo '<p>Description: <b>' . $oIMDB->getDescription() . '</b></p>';
-        echo '<p>Directors: <b>' . $oIMDB->getDirector() . '</b></p>';
-        echo '<p>Directors as URL: <b>' . $oIMDB->getDirectorAsUrl() . '</b></p>';
-        echo '<p>Genres: <b>' . $oIMDB->getGenre() . '</b></p>';
-        echo '<p>Genres as URL: <b>' . $oIMDB->getGenreAsUrl() . '</b></p>';
-        echo '<p>Languages: <b>' . $oIMDB->getLanguages() . '</b></p>';
-        echo '<p>Languages as URL: <b>' . $oIMDB->getLanguagesAsUrl() . '</b></p>';
-        echo '<p>Location: <b>' . $oIMDB->getLocation() . '</b></p>';
-        echo '<p>Location as URL: <b>' . $oIMDB->getLocationAsUrl() . '</b></p>';
-        echo '<p>MPAA: <b>' . $oIMDB->getMpaa() . '</b></p>';
-        echo '<p>Opening Weekend: <b>' . $oIMDB->getOpening() . '</b></p>';
-        echo '<p>Plot (shortened to 150 chars): <b>' . $oIMDB->getPlot(150) . '</b></p>';
-        echo '<p>Poster: <b>' . $oIMDB->getPoster((($i % 2) == 0) ? 'big' : 'small') . '</b></p>';
-        echo '<p>Rating: <b>' . $oIMDB->getRating() . '</b></p>';
-        echo '<p>Release Date: <b>' . $oIMDB->getReleaseDate() . '</b></p>';
-        echo '<p>Runtime: <b>' . $oIMDB->getRuntime() . '</b></p>';
-        echo '<p>Seasons: <b>' . $oIMDB->getSeasons() . '</b></p>';
-        echo '<p>Seasons as URL: <b>' . $oIMDB->getSeasonsAsUrl() . '</b></p>';
-        echo '<p>Sound Mix: <b>' . $oIMDB->getSoundMix() . '</b></p>';
-        echo '<p>Sites as URL: <b>' . $oIMDB->getSitesAsUrl('_blank') . '</b></p>';
-        echo '<p>Tagline: <b>' . $oIMDB->getTagline() . '</b></p>';
-        echo '<p>Title: <b>' . $oIMDB->getTitle() . '</b></p>';
-        if ($oIMDB->getTrailerAsUrl() != $oIMDB->strNotFound) {
-            echo '<p>Trailer: <b><a href="' . $oIMDB->getTrailerAsUrl() . '">' . $oIMDB->getTrailerAsUrl() . '</a></b></p>';
+        echo '<h1>' . $sMovie . '</h1>';
+        foreach ($oIMDB->getAll() as $aItem) {
+            echo '<p><b>' . $aItem['name'] . '</b>: ' . $aItem['value'] . '</p>';
         }
-        else {
-            echo '<p>Trailer: <b>' . $oIMDB->getTrailerAsUrl() . '</b></p>';
-        }
-        echo '<p>Url: <b><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getUrl() . '</a></b></p>';
-        echo '<p>Votes: <b>' . $oIMDB->getVotes() . '</b></p>';
-        echo '<p>Writers: <b>' . $oIMDB->getWriter() . '</b></p>';
-        echo '<p>Writers as URL: <b>' . $oIMDB->getWriterAsUrl() . '</b></p>';
-        echo '<p>Year: <b>' . $oIMDB->getYear() . '</b></p>';
     }
     else {
-        echo '<p>Movie not found: ' . $strTest . '</p>';
+        echo '<p><b>Movie not found</b>: ' . $sMovie . '</p>';
     }
     echo '<hr>';
 }
