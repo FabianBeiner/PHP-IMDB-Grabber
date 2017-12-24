07ee41dea6edf9bc06fd563e7ef266ffe386c090
Fabian Beiner <fb@fabianbeiner.de>
2017-12-24 18:05:40 +0100

    Updated example files.

--- examples/imdb.example.php
+++ examples/imdb.example.php
@@ -1,48 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>PHP-IMDB-Grabber by Fabian Beiner | Example</title>
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,700">
-    <style>
-        body {
-            background-color: #E5E5E5;
-            color:            #222;
-            font-family:      "Open Sans", sans-serif;
-            font-size:        15px;
-            max-width:        1000px;
-            margin:           20px auto;
-            width:            100%;
-        }
+  <meta charset="utf-8">
+  <title>PHP-IMDB-Grabber by Fabian Beiner | Example</title>
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,700">
+  <style>
+    body {
+      background-color: #E5E5E5;
+      color: #222;
+      font-family: "Open Sans", sans-serif;
+      font-size: 15px;
+      max-width: 1000px;
+      margin: 20px auto;
+      width: 100%;
+    }
 
-        p {
-            margin:  0 0 10px;
-            padding: 0;
-        }
+    p {
+      margin: 0 0 10px;
+      padding: 0;
+    }
 
-        hr {
-            clear:      both;
-            margin:     25px 0;
-            border:     1px #000 solid;
-            height:     1px;
-            background: #FFF;
-        }
+    hr {
+      clear: both;
+      margin: 25px 0;
+      border: 1px #000 solid;
+      height: 1px;
+      background: #FFF;
+    }
 
-        a {
-            color: #222;
-        }
+    a {
+      color: #222;
+    }
 
-        a:hover, a:focus, a:active {
-            text-decoration: none;
-            color:           #222;
-        }
+    a:hover, a:focus, a:active {
+      text-decoration: none;
+      color: #222;
+    }
 
-        h1 {
-            font-size:   32px;
-            text-align:  center;
-            font-weight: 700;
-        }
-    </style>
+    h1 {
+      font-size: 32px;
+      text-align: center;
+      font-weight: 700;
+    }
+  </style>
 </head>
 <body>
 <?php
@@ -50,7 +50,13 @@ include_once '../imdb.class.php';
 
 $oIMDB = new IMDB('http://us.imdb.com/Title?0144117');
 if ($oIMDB->isReady) {
-    echo '<p><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.</p>';
+    echo '<p><a href="' .
+         $oIMDB->getUrl() .
+         '">' .
+         $oIMDB->getTitle() .
+         '</a> got rated ' .
+         $oIMDB->getRating() .
+         '.</p>';
 } else {
     echo '<p>Movie not found!</p>';
 }
@@ -77,8 +83,18 @@ if ($oIMDB->isReady) {
 <?php
 $oIMDB = new IMDB('http://www.imdb.com/title/tt1022603/');
 if ($oIMDB->isReady) {
-    echo '<p><a href="' . $oIMDB->getUrl() . '">' . $oIMDB->getTitle() . '</a> got rated ' . $oIMDB->getRating() . '.</p>';
-    echo '<p><img src="../' . $oIMDB->getPoster('small', true) . '" style="float:left;margin:4px 10px 10px 0;"> <b>About the movie:</b> ' . $oIMDB->getPlot() . '</p>';
+    echo '<p><a href="' .
+         $oIMDB->getUrl() .
+         '">' .
+         $oIMDB->getTitle() .
+         '</a> got rated ' .
+         $oIMDB->getRating() .
+         '.</p>';
+    echo '<p><img src="../' .
+         $oIMDB->getPoster('small', true) .
+         '" style="float:left;margin:4px 10px 10px 0;"> <b>About the movie:</b> ' .
+         $oIMDB->getPlot() .
+         '</p>';
 } else {
     echo '<p>Movie not found!</p>';
 }
